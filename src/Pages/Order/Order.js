c47e1de4ebec2c10bb033cd7ade7ddcdee24f38b
jahidhasan234031 <jahidhasan234031@mail.com>
2022-06-05 11:30:36 +0600

    first commit

--- src/Pages/Order/Order.js
+++ src/Pages/Order/Order.js
@@ -9,18 +9,18 @@ const Order = () => {
     const [user] = useAuthState(auth);
     const [orders, setOrders] = useState([]);
     const navigate = useNavigate();
-    useEffect( () => {
-        
-        const getOrders = async() =>{
+    useEffect(() => {
+
+        const getOrders = async () => {
             const email = user.email;
             const url = `http://localhost:5000/order?email=${email}`;
-            try{
-                const {data} = await axiosPrivate.get(url);
+            try {
+                const { data } = await axiosPrivate.get(url);
                 setOrders(data);
             }
-            catch(error){
+            catch (error) {
                 console.log(error.message);
-                if(error.response.status === 401 || error.response.status === 403){
+                if (error.response.status === 401 || error.response.status === 403) {
                     signOut(auth);
                     navigate('/login')
                 }
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,17 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:process.env.REACT_APP_apiKey,
-  authDomain:process.env.REACT_APP_authDomain,
-  projectId:process.env.REACT_APP_projectId,
-  storageBucket:process.env.REACT_APP_storageBucket,
-  messagingSenderId:process.env.REACT_APP_messagingSenderId,
-  appId:process.env.REACT_APP_appId,
+  apiKey: "AIzaSyD2dmPYW80-ikqPdh1BVF8gms04CwWuq-k",
+  authDomain: "genius-car-services-292c3.firebaseapp.com",
+  projectId: "genius-car-services-292c3",
+  storageBucket: "genius-car-services-292c3.appspot.com",
+  messagingSenderId: "372543369928",
+  appId: "1:372543369928:web:4d235c9a960d9018a6abfd"
 };
 
 // Initialize Firebase
